6592f15702bcee32e7c0c4bc3fbeb084dda69166
jailsonpj <jpj.ads@uea.edu.br>
2019-10-01 03:29:38 -0400

    remove notebooks

---
+++ python/introducao/exer.py
@@ -0,0 +1,12 @@
+def fat(n):
+    resul = 1
+    for i in range(2,n+1):
+        resul *= i
+    return resul
+
+
+def primo(n):
+    for i in range(2,n):
+        if n%i == 0
+            return False
+    return True
